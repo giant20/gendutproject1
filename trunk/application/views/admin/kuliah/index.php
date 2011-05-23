46359e8b8a45e4bb071b5cb07683ab3ffc7e98fe
areakerja@gmail.com <areakerja@gmail.com@19be8438-2533-f030-10a6-26158b3e041e>
2011-05-23 04:32:02 +0000

    upload manual

---
+++ trunk/application/views/admin/kuliah/index.php
@@ -0,0 +1,18 @@
+<h1>Data Kuliah</h1>
+<br /><br />
+<table width="500" border="0" cellpadding="0" cellspacing="0">
+<tr><th>No</th><th>No Mahasiswa</th><th>Nama Siswa</th>
+<th>Aksi</th></tr>
+<?php $no=1+$urutan;?>
+<?php foreach ($query as $row){?>
+<?php if ($no %2 == 0)?>
+<tr class="<?php if ($no %2 == 0) echo 'zebra';?>">
+        <td><?php echo $no?></td>
+        <td><?php echo $row['no_mahasiswa']?></td>
+        <td><?php echo $row['nama_mahasiswa']?></td>
+
+        <td><a href="<?php echo base_url().'admin/kuliah/view/'.$row['id_mahasiswa'];?>">view</a></td>
+</tr>
+        <?php $no++;}?>
+</table>
+                        <?php echo $this->pagination->create_links();?>
